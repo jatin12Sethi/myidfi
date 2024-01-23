23b14e9f4e5fe665b78eff554b9adcb9e665a432
jatin sethi <98033741+jatin12Sethi@users.noreply.github.com>
2024-01-23 22:53:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def root(request: chatBOT):
     k = application.query(request.prompt)
     return {'Message' : k}
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+#if __name__ == '__main__':
+#   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 #python -m main.py
